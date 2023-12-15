5802295ae8f66eb6a2243b66e93c73dccf4cd0cd
Ajay parihar <ajayparihar876@gmail.com>
2023-12-15 19:26:15 +0530

    add: upload,chat,profile page's

--- post/urls.py
+++ post/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from post import views
 
 urlpatterns = [
-    path('',views.index, name = "index")
+    path('',views.index, name = "index"),
+    path('upload/',views.upload, name = "upload"),
+    path('chat/',views.chat, name = "chat"),
+    path('profile/',views.profile, name = "profile"),
 ]
--- post/views.py
+++ post/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def upload(request):
+    return render(request,'upload/upload.html')
+
+def chat(request):
+    return render(request,'chat/chat.html')
+
+def profile(request):
+    return render(request,"profile/profile.html")
