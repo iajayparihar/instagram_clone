c7253b0caa32d7101c973c51defa983f9a1297ee
Ajay parihar <ajayparihar876@gmail.com>
2024-01-31 12:50:41 +0530

    fix: chat content

--- post/models.py
+++ post/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
     bio = models.TextField(blank=True)
     link = models.URLField(blank=True)
     profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
